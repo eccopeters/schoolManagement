ce1a033b5b7dd7f591b55e1917c7dcde9d2c0c05
eccopeters <pekojikoko123@gmail.com>
2025-08-27 01:14:17 +0100

    Fix Railway Mysql connection issues

--- config/db.js
+++ config/db.js
@@ -1,17 +1,17 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 
 export const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASS,
-  database: process.env.DATABASE
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
-
-db.connect(error =>{
-  if(error){
-    console.error("Database Connection failed: ", error.message)
-    process.exit(1)
+db.connect((error) => {
+  if (error) {
+    console.error("Database Connection failed: ", error.message);
+    process.exit(1);
   }
-  console.log("Database connected successfully!")
-})
+  console.log("Database connected successfully!");
+});
